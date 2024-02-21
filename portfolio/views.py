4f47c08803a835640a24240086cf3f350b419aa0
Ikram Alam <ikramalam515@gmail.com>
2024-02-21 11:44:11 +0500

    Your commit message here

---
+++ portfolio/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from portfolio.models import Contact,Blogs,Achievement,Project
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def handleblog(request):
+    posts=Blogs.objects.all()
+    context={"posts":posts}
+    return render(request,'handleblog.html',context)
+
+
+def handleproject(request):
+    post2=Project.objects.all()
+    context={"posts":post2}
+    return render(request,'handleproject.html',context)
+
+
+
+
+def handleachievement(request):
+    posts=Achievement.objects.all()
+    context={"post1":posts}
+    return render(request,'handleachievement.html',context)
+
+
+def about(request):
+    return render(request,'about.html')
+
+
+
+def contact(request):
+    if request.method=="POST":
+        fname=request.POST.get('name')
+        femail=request.POST.get('email')
+        fphoneno=request.POST.get('num')
+        fdesc=request.POST.get('desc')
+        query=Contact(name=fname,email=femail,phonenumber=fphoneno,description=fdesc)
+        query.save()
+        messages.success(request,"Thanks for contacting us. We will get by you Soon!")
+
+        return redirect('/contact')
+
+    return render(request,'contact.html')
+
+
